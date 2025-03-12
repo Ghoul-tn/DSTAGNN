0b7fed01d0e4ac522f963942628b044003edf643
Ghoul-tn <67835611+Ghoul-tn@users.noreply.github.com>
2025-03-12 02:28:01 +0100

    Update prepareData.py

--- prepareData.py
+++ prepareData.py
@@ -4,108 +4,6 @@ import argparse
 import configparser
 
 
-def search_data(sequence_length, num_of_depend, label_start_idx,
-                num_for_predict, units, points_per_hour):
-    '''
-    Parameters
-    ----------
-    sequence_length: int, length of all history data
-    num_of_depend: int,
-    label_start_idx: int, the first index of predicting target
-    num_for_predict: int, the number of points will be predicted for each sample
-    units: int, week: 7 * 24, day: 24, recent(hour): 1
-    points_per_hour: int, number of points per hour, depends on data
-    Returns
-    ----------
-    list[(start_idx, end_idx)]
-    '''
-    if points_per_hour < 0:
-        raise ValueError("points_per_hour should be greater than 0!")
-
-    if label_start_idx + num_for_predict > sequence_length:
-        return None
-
-    x_idx = []
-    for i in range(1, num_of_depend + 1):
-        start_idx = label_start_idx - points_per_hour * units * i
-        end_idx = start_idx + num_for_predict
-        if start_idx >= 0:
-            x_idx.append((start_idx, end_idx))
-        else:
-            return None
-
-    if len(x_idx) != num_of_depend:
-        return None
-
-    return x_idx[::-1]
-
-
-def get_sample_indices(data_sequence, num_of_weeks, num_of_days, num_of_hours,
-                       label_start_idx, num_for_predict, points_per_hour=12):
-    '''
-    Parameters
-    ----------
-    data_sequence: np.ndarray
-                   shape is (sequence_length, num_of_vertices, num_of_features)
-    num_of_weeks, num_of_days, num_of_hours: int
-    label_start_idx: int, the first index of predicting target
-    num_for_predict: int,
-                     the number of points will be predicted for each sample
-    points_per_hour: int, default 12, number of points per hour
-    Returns
-    ----------
-    week_sample: np.ndarray
-                 shape is (num_of_weeks * points_per_hour,
-                           num_of_vertices, num_of_features)
-    day_sample: np.ndarray
-                 shape is (num_of_days * points_per_hour,
-                           num_of_vertices, num_of_features)
-    hour_sample: np.ndarray
-                 shape is (num_of_hours * points_per_hour,
-                           num_of_vertices, num_of_features)
-    target: np.ndarray
-            shape is (num_for_predict, num_of_vertices, num_of_features)
-    '''
-    week_sample, day_sample, hour_sample = None, None, None
-
-    if label_start_idx + num_for_predict > data_sequence.shape[0]:
-        return week_sample, day_sample, hour_sample, None
-
-    if num_of_weeks > 0:
-        week_indices = search_data(data_sequence.shape[0], num_of_weeks,
-                                   label_start_idx, num_for_predict,
-                                   7 * 24, points_per_hour)
-        if not week_indices:
-            return None, None, None, None
-
-        week_sample = np.concatenate([data_sequence[i: j]
-                                      for i, j in week_indices], axis=0)
-
-    if num_of_days > 0:
-        day_indices = search_data(data_sequence.shape[0], num_of_days,
-                                  label_start_idx, num_for_predict,
-                                  24, points_per_hour)
-        if not day_indices:
-            return None, None, None, None
-
-        day_sample = np.concatenate([data_sequence[i: j]
-                                     for i, j in day_indices], axis=0)
-
-    if num_of_hours > 0:
-        hour_indices = search_data(data_sequence.shape[0], num_of_hours,
-                                   label_start_idx, num_for_predict,
-                                   1, points_per_hour)
-        if not hour_indices:
-            return None, None, None, None
-
-        hour_sample = np.concatenate([data_sequence[i: j]
-                                      for i, j in hour_indices], axis=0)
-
-    target = data_sequence[label_start_idx: label_start_idx + num_for_predict]
-
-    return week_sample, day_sample, hour_sample, target
-
-
 def read_and_generate_dataset(graph_signal_matrix_filename,
                               num_of_weeks, num_of_days, num_of_hours,
                               num_for_predict, points_per_hour=12, save=False):
@@ -152,50 +50,18 @@ def read_and_generate_dataset(graph_signal_matrix_filename,
     val_target = target[train_size:train_size + val_size]
     test_target = target[train_size + val_size:]
 
-    # Compute mean and std for normalization
-    mean = train_data.mean(axis=(0, 1, 3), keepdims=True)
-    std = train_data.std(axis=(0, 1, 3), keepdims=True)
-
     # Save the datasets (optional)
     if save:
         file = os.path.basename(graph_signal_matrix_filename).split('.')[0]
-        dirpath = os.path.dirname("/kaggle/working/")
+        dirpath = os.path.dirname(graph_signal_matrix_filename)
         filename = os.path.join(dirpath, f"{file}_r{num_of_hours}_d{num_of_days}_w{num_of_weeks}_dstagnn")
         print('save file:', filename)
         np.savez_compressed(filename,
                             train_x=train_data, train_target=train_target,
                             val_x=val_data, val_target=val_target,
-                            test_x=test_data, test_target=test_target,
-                            mean=mean, std=std)
+                            test_x=test_data, test_target=test_target)
 
     return train_data, val_data, test_data, train_target, val_target, test_target
-
-
-def normalization(train, val, test):
-    '''
-    Parameters
-    ----------
-    train, val, test: np.ndarray (B,N,F,T)
-    Returns
-    ----------
-    stats: dict, two keys: mean and std
-    train_norm, val_norm, test_norm: np.ndarray,
-                                     shape is the same as original
-    '''
-    assert train.shape[1:] == val.shape[1:] and val.shape[1:] == test.shape[1:]  # ensure the num of nodes is the same
-    mean = train.mean(axis=(0, 1, 3), keepdims=True)
-    std = train.std(axis=(0, 1, 3), keepdims=True)
-    print('mean.shape:', mean.shape)
-    print('std.shape:', std.shape)
-
-    def normalize(x):
-        return (x - mean) / std
-
-    train_norm = normalize(train)
-    val_norm = normalize(val)
-    test_norm = normalize(test)
-
-    return {'_mean': mean, '_std': std}, train_norm, val_norm, test_norm
 
 
 # Prepare dataset
