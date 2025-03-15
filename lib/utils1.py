2726cc892ae4ffb54c4a2e8fc5b3d1a2ac146a8b
Ghoul-tn <67835611+Ghoul-tn@users.noreply.github.com>
2025-03-15 11:31:04 +0100

    Update utils1.py

--- lib/utils1.py
+++ lib/utils1.py
@@ -291,83 +291,83 @@ def calculate_laplacian_matrix(adj_mat, mat_type):
             return hat_rw_normd_lap_mat
 
 
-def load_graphdata_channel1(graph_signal_matrix_filename, num_of_hours, num_of_days, num_of_weeks, DEVICE, batch_size, shuffle=True):
-    '''
-    这个是为PEMS的数据准备的函数
-    将x,y都处理成归一化到[-1,1]之前的数据;
-    每个样本同时包含所有监测点的数据，所以本函数构造的数据输入时空序列预测模型；
-    该函数会把hour, day, week的时间串起来；
-    注： 从文件读入的数据，x是最大最小归一化的，但是y是真实值
-    这个函数转为mstgcn，astgcn设计，返回的数据x都是通过减均值除方差进行归一化的，y都是真实值
-    :param graph_signal_matrix_filename: str
-    :param num_of_hours: int
-    :param num_of_days: int
-    :param num_of_weeks: int
-    :param DEVICE:
-    :param batch_size: int
-    :return:
-    three DataLoaders, each dataloader contains:
-    test_x_tensor: (B, N_nodes, in_feature, T_input)
-    test_decoder_input_tensor: (B, N_nodes, T_output)
-    test_target_tensor: (B, N_nodes, T_output)
+def load_graphdata_channel1(graph_signal_matrix_filename, num_of_hours, num_of_days, num_of_weeks, device, batch_size):
+    """
+    Load and preprocess the dataset for DSTAGNN.
+    Args:
+        graph_signal_matrix_filename: Path to the .npz file containing the dataset.
+        num_of_hours: Number of past timesteps to use for prediction.
+        num_of_days: Number of past days to use for prediction.
+        num_of_weeks: Number of past weeks to use for prediction.
+        device: Device to load the data onto (e.g., 'cuda:0' or 'cpu').
+        batch_size: Batch size for the data loader.
+    Returns:
+        Data loaders and tensors for training, validation, and testing.
+    """
+    # Load the dataset
+    data = np.load(graph_signal_matrix_filename)
+    input_features = data['data']  # Shape: (num_timesteps, num_nodes, num_features)
+    target = data['target']  # Shape: (num_timesteps, num_nodes)
 
-    '''
+    # Ensure the data is in the correct shape
+    if len(input_features.shape) != 3:
+        raise ValueError("Input data must have shape (num_timesteps, num_nodes, num_features)")
+    if len(target.shape) != 2:
+        raise ValueError("Target data must have shape (num_timesteps, num_nodes)")
 
-    file = os.path.basename(graph_signal_matrix_filename).split('.')[0]
+    # Split the data into training, validation, and test sets
+    num_samples = input_features.shape[0]
+    train_size = int(num_samples * 0.6)
+    val_size = int(num_samples * 0.2)
 
-    dirpath = os.path.dirname(graph_signal_matrix_filename)
+    train_data = input_features[:train_size]  # Shape: (train_size, num_nodes, num_features)
+    val_data = input_features[train_size:train_size + val_size]  # Shape: (val_size, num_nodes, num_features)
+    test_data = input_features[train_size + val_size:]  # Shape: (test_size, num_nodes, num_features)
 
-    filename = os.path.join(dirpath,
-                            file + '_r' + str(num_of_hours) + '_d' + str(num_of_days) + '_w' + str(num_of_weeks)) +'_dstagnn'
+    train_target = target[:train_size]  # Shape: (train_size, num_nodes)
+    val_target = target[train_size:train_size + val_size]  # Shape: (val_size, num_nodes)
+    test_target = target[train_size + val_size:]  # Shape: (test_size, num_nodes)
 
-    print('load file:', filename)
+    # Normalize the data
+    mean = train_data.mean(axis=(0, 1), keepdims=True)  # Shape: (1, 1, num_features)
+    std = train_data.std(axis=(0, 1), keepdims=True)  # Shape: (1, 1, num_features)
 
-    file_data = np.load(filename + '.npz')
-    train_x = file_data['train_x']  # (10181, 307, 3, 12)
-    train_x = train_x[:, :, 0:1, :]
-    train_target = file_data['train_target']  # (10181, 307, 12)
+    def normalize(x):
+        return (x - mean) / std
 
-    val_x = file_data['val_x']
-    val_x = val_x[:, :, 0:1, :]
-    val_target = file_data['val_target']
+    train_data = normalize(train_data)
+    val_data = normalize(val_data)
+    test_data = normalize(test_data)
 
-    test_x = file_data['test_x']
-    test_x = test_x[:, :, 0:1, :]
-    test_target = file_data['test_target']
+    # Reshape the data to 4D: (num_samples, num_nodes, num_features, num_timesteps)
+    train_data = np.transpose(train_data, (1, 2, 0))  # Shape: (num_nodes, num_features, train_size)
+    train_data = np.expand_dims(train_data, axis=0)  # Shape: (1, num_nodes, num_features, train_size)
 
-    mean = file_data['mean'][:, :, 0:1, :]  # (1, 1, 3, 1)
-    std = file_data['std'][:, :, 0:1, :]  # (1, 1, 3, 1)
+    val_data = np.transpose(val_data, (1, 2, 0))  # Shape: (num_nodes, num_features, val_size)
+    val_data = np.expand_dims(val_data, axis=0)  # Shape: (1, num_nodes, num_features, val_size)
 
-    # ------- train_loader -------
-    train_x_tensor = torch.from_numpy(train_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
-    train_target_tensor = torch.from_numpy(train_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
+    test_data = np.transpose(test_data, (1, 2, 0))  # Shape: (num_nodes, num_features, test_size)
+    test_data = np.expand_dims(test_data, axis=0)  # Shape: (1, num_nodes, num_features, test_size)
 
-    train_dataset = torch.utils.data.TensorDataset(train_x_tensor, train_target_tensor)
+    # Convert to PyTorch tensors
+    train_data = torch.FloatTensor(train_data).to(device)
+    val_data = torch.FloatTensor(val_data).to(device)
+    test_data = torch.FloatTensor(test_data).to(device)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    train_target = torch.FloatTensor(train_target).to(device)
+    val_target = torch.FloatTensor(val_target).to(device)
+    test_target = torch.FloatTensor(test_target).to(device)
 
-    # ------- val_loader -------
-    val_x_tensor = torch.from_numpy(val_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
-    val_target_tensor = torch.from_numpy(val_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
+    # Create data loaders
+    train_dataset = torch.utils.data.TensorDataset(train_data, train_target)
+    val_dataset = torch.utils.data.TensorDataset(val_data, val_target)
+    test_dataset = torch.utils.data.TensorDataset(test_data, test_target)
 
-    val_dataset = torch.utils.data.TensorDataset(val_x_tensor, val_target_tensor)
-
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
-    # ------- test_loader -------
-    test_x_tensor = torch.from_numpy(test_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
-    test_target_tensor = torch.from_numpy(test_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
-
-    test_dataset = torch.utils.data.TensorDataset(test_x_tensor, test_target_tensor)
-
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    # print
-    print('train:', train_x_tensor.size(), train_target_tensor.size())
-    print('val:', val_x_tensor.size(), val_target_tensor.size())
-    print('test:', test_x_tensor.size(), test_target_tensor.size())
-
-    return train_x_tensor, train_loader, train_target_tensor, val_x_tensor, val_loader, val_target_tensor, test_x_tensor, test_loader, test_target_tensor, mean, std
+    return train_data, train_loader, train_target, val_data, val_loader, val_target, test_data, test_loader, test_target, mean, std
 
 
 def compute_val_loss_mstgcn(net, val_loader, criterion, sw, epoch, limit=None):
